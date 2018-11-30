5b47b35333ae25c9a6f87de4a9a464fcee9f04e2
srtekMAC <srtekmac@srtekmacs-macbook-air.local>
2018-11-30 11:50:25 +0530

    b1vc1 cmmi

---
+++ test/B1ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  B1ViewController.swift
+//  test
+//
+//  Created by srtekMAC on 30/11/18.
+//  Copyright © 2018 srtekMAC. All rights reserved.
+//
+
+import UIKit
+
+class B1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("b1vc1")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
